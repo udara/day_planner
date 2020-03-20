7ffb52092a10f0b04ad567159de89e1219c95e53
Udara ranasinghe <indi79@hotmail.com>
2020-03-20 12:24:06 +1030

    style.css - adding look and feel styles js/day_planner - adding date at the to

--- js/day_planner.js
+++ js/day_planner.js
@@ -1,7 +1,7 @@
 if (localStorage.getItem('day_planner')){
     var  day_planner =  JSON.parse(localStorage.getItem('day_planner'));
  }
- else{
+ else {
     var day_planner = [
         { time: "0900", events: "" },{ time: "1000", events: "" },{ time: "1100", events: "" },
         { time: "1200", events: "" },{ time: "1300", events: "" },{ time: "1400", events: "" },
@@ -9,23 +9,16 @@ if (localStorage.getItem('day_planner')){
       ];
  }
 
-function renderTimeSlot(time,events)
-{
+function renderTimeSlot(time,events) {
     var time_slot = "";
     var time_ampm = moment(time, "H").format("h:mm A");
     var bg_color = "";
-    if(moment().format("H00") > time){
-        bg_color = 'past';
-    }
-    else if(moment().format("H00") == time){
-        bg_color = 'present';
-    }
-    else{
-        bg_color = 'future';
-    }
+    if(moment().format("H00") > time){ bg_color = 'past';}
+    else if(moment().format("H00") == time){ bg_color = 'present'; }
+    else{ bg_color = 'future';}
 
     time_slot = `<div class="row timeslot" id="${time}_time_slot">
-    <div class="col-3 col-md-2 col-xl-1  hour" id="${time}">${time_ampm}</div>
+    <div class="col-3 col-md-2 col-xl-1 hour" id="${time}">${time_ampm}</div>
     <div class="col-7 col-md-9 col-xl-10 hourly_events ${bg_color}" contentEditable="true" id="${time}_events">${events}</div>
     <div class="col-2 col-md-1 col-xl-1 btn_save"><i class="fa fa-floppy-o"></i></div>
     </div>`;
@@ -54,3 +47,8 @@ $('.btn_save').on( "click", function() {
     }
     localStorage.setItem('day_planner', JSON.stringify(day_planner));
 });
+
+$( document ).ready(function() {
+$('#currentDay').html(moment().format("MMM Do YY"));
+});
+
